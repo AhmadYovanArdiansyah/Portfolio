b857ea9aec827b9964edaca64d96955075f8f3d6
ahmadyovanardiansyah <ahmadyovanardiansyah@gmail.com>
2023-07-31 18:51:41 +0700

    add p-20 instead text-center

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 const About = () => {
     return (
         <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{duration: .5}} className="h-full w-full absolute flex flex-col pt-24 gap-14">
-            <motion.div className=" text-2xl font-play text-gray-200 mx-32">
+            <motion.div className=" text-2xl font-play text-gray-200 mx-32 pl-20">
                 <p>
                     Hello there! I m Ahmad Yovan, a passionate web developer with a flair for creativity and a background in various programming languages. Although I m relatively new to web development, I ve already built a strong foundation in HTML, CSS, and JavaScript.
                 </p>
